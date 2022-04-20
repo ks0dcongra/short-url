2e26273c0a6ca8b71d8f8a4dd4fefb74f1d9fb6b
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-21 05:10:26 +0800

    feat: add shortUrl seeder

---
+++ models/seeds/shortUrlSeeder.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const ShortUrl = require('../shortUrl')
+mongoose.connect('mongodb+srv://alpha:camp@cluster0.ctbuq.mongodb.net/todo-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connection!')
+  for (let i = 0; i < 10; i++) {
+    ShortUrl.create({ name: `name-${i}` })
+  }
+  console.log('done')
+})
