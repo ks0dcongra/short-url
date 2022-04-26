f6c9798883e0b4c599c8cf9f8905ad4999c1ae41
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-26 09:24:44 +0800

    perf: delete try catch

--- utils/tryHttp.js
+++ utils/tryHttp.js
@@ -1,16 +1,13 @@
 // 判斷輸入的開頭是否為http:// 或 https://
-try {
-  function tryHttp (data) {
-    const regExp = '(^http\:\/\/|^https\:\/\/)(.)+'
-    const result = data.match(regExp)
-    if (!result) {
-      return false
-    } else {
-      return true
-    }
+function tryHttp(data) {
+  const regExp = '(^http\:\/\/|^https\:\/\/)(.)+'
+  const result = data.match(regExp)
+  if (!result) {
+    return false
+  } else {
+    return true
   }
-} catch {
-  console.log(Error)
 }
+
 
 module.exports = tryHttp
